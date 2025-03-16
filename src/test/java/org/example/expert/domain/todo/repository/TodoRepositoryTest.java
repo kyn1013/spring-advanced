95fc58c4a6d669c0c4fa9238f30bd38577441a04
kyn1013 <yn112511@naver.com>
2025-03-17 01:11:49 +0900

    test : todo와 작성자 함께 조회

---
+++ src/test/java/org/example/expert/domain/todo/repository/TodoRepositoryTest.java
@@ -0,0 +1,49 @@
+package org.example.expert.domain.todo.repository;
+
+import org.example.expert.client.WeatherClient;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class TodoRepositoryTest {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void Todo와_작성자를_함께_조회한다() {
+        // given
+        String email = "yn@abc.com";
+        User user = new User(email, "password", UserRole.USER);
+        userRepository.save(user);
+
+        String weather = "맑음";
+
+        Todo todo = new Todo(
+                "할일 제목",
+                "할일 내용",
+                weather,
+                user
+        );
+        Todo savedTodo = todoRepository.save(todo);
+
+        // when
+        Todo foundTodo = todoRepository.findByIdWithUser(savedTodo.getId()).orElse(null);
+
+        // then
+        assertNotNull(foundTodo);
+        assertEquals("할일 제목", foundTodo.getTitle());
+        assertEquals("할일 내용", foundTodo.getContents());
+        assertEquals("yn@abc.com", foundTodo.getUser().getEmail());
+    }
+}
--- src/test/java/org/example/expert/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/org/example/expert/domain/user/repository/UserRepositoryTest.java
@@ -1,7 +1,49 @@
 package org.example.expert.domain.user.repository;
 
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void 이메일로_사용자를_조회할_수_있다() {
+        // given
+        String email = "yn@abc.com";
+        User user = new User(email, "password", UserRole.USER);
+        userRepository.save(user);
+
+        // when
+        User foundUser = userRepository.findByEmail(email).orElse(null);
+
+        // then
+        assertNotNull(foundUser);
+        assertEquals(email, foundUser.getEmail());
+        assertEquals(UserRole.USER, foundUser.getUserRole());
+    }
+
+    @Test
+    public void 이메일이_존재하는지_확인할_수_있다() {
+        // given
+        String email = "yn@abc.com";
+        User user = new User(email, "password", UserRole.USER);
+        userRepository.save(user);
+
+        // when
+        Boolean emailExists = userRepository.existsByEmail(email);
+
+        // then
+        assertNotNull(emailExists);
+        assertEquals(true, emailExists);
+    }
+
 
 }
